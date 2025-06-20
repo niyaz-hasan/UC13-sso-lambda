af5f0bbe6079632a071591ea3f36d15fd4779d16
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-20 11:54:09 +0530

    terraform check

--- modules/cognito/outputs.tf
+++ modules/cognito/outputs.tf
@@ -6,6 +6,10 @@ output "client_id" {
   value = aws_cognito_user_pool_client.client.id
 }
 
-output "user_pool_endpoint" {
-  value = "https://cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.this.id}"
+#output "user_pool_endpoint" {
+#  value = "https://cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.this.id}"
+#}
+
+output "user_pool_arn" {
+  value = aws_cognito_user_pool.this.arn
 }
