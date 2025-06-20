f1f21fd4351ebb73509bc611fba0d8f5cedeaf8e
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-20 12:44:49 +0530

    added previous change

--- modules/cognito/outputs.tf
+++ modules/cognito/outputs.tf
@@ -6,10 +6,10 @@ output "client_id" {
   value = aws_cognito_user_pool_client.client.id
 }
 
-#output "user_pool_endpoint" {
-#  value = "https://cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.this.id}"
-#}
-
 output "user_pool_endpoint" {
-  value = aws_cognito_user_pool.this.endpoint 
+  value = "https://cognito-idp.${var.region}.amazonaws.com/${aws_cognito_user_pool.this.id}"
 }
+
+#output "user_pool_endpoint" {
+#  value = aws_cognito_user_pool.this.endpoint 
+#}
